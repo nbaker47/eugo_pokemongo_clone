2223b6385a319f9d003c9e3c8bcb90a7aedbeed6
nbaker47 <nathan.baker.2647@gmail.com>
2022-02-15 11:50:21 +0000

    added the cover page, bootstrap CSS, favicon, and some JS animation files

---
+++ eugo/static/eugo/js/draw_small.js
@@ -0,0 +1,25 @@
+var c = document.getElementById("canvas");
+fitToContainer(c);
+var ctx = c.getContext("2d");
+var i = 0;
+setInterval(function(){draw(ctx)}, 10);
+
+function draw(ctx){
+    var w = canvas.width;
+    var h = canvas.height;
+    ctx.strokeStyle = "#9f9f9f";
+    ctx.beginPath();
+    ctx.moveTo(2+i,3*i)
+    ctx.lineTo(w-i,h-i)
+    ctx.stroke();
+    i++;
+}
+
+function fitToContainer(canvas){
+    // Make it visually fill the positioned parent
+    canvas.style.width ='100%';
+    canvas.style.height='100%';
+    // ...then set the internal size to match
+    canvas.width  = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+}
--- eugo/views.py
+++ eugo/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
