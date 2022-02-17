604f17aaf9b7b4bc68bfd7fb567b20f6f5972475
nbaker47 <nathan.baker.2647@gmail.com>
2022-02-17 17:20:27 +0000

    added html templates, and began work on map.html/css

--- eugo/urls.py
+++ eugo/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('map/', views.map, name='map'),
+    path('battle/', views.battle, name='battle'),
 ]
--- eugo/views.py
+++ eugo/views.py
@@ -7,3 +7,12 @@ from django.shortcuts import get_object_or_404
 
 def index(request):
     return render(request, 'index.html')
+
+def battle(request):
+    return render(request, 'battle.html')
+
+def map(request):
+    return render(request, 'map.html')
+
+def login(request):
+    return render(request, 'login.html')
