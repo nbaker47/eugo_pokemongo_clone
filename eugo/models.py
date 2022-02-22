3da1213915165fef74c2312d3bec9214efef75cc
nbaker47 <nathan.baker.2647@gmail.com>
2022-02-22 01:08:16 +0000

    madmod alomst done, starting making table for Lecturer - finish it georgi

--- eugo/models.py
+++ eugo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Lecturer(models.Model):
+    name = models.CharField(max_length=255)
+    duration = models.SlugField(unique=True, max_length=255)
+    hp = models.TextField()
+    attack = models.DateTimeField(auto_now_add=True)
+    subject = models.TextField()
+    sprite = models.TextField()
+    pos = models.TextField()
+    lecID = models.TextField()
