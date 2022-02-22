3fc9b2b3ec3524fdd60107af8cd6b15f60d9301f
nbaker47 <nathan.baker.2647@gmail.com>
2022-02-22 18:36:59 +0000

    finished map/mapmod and made table for lecturers - mods can now create lecturers for normal users to see on the map

--- eugo/models.py
+++ eugo/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Lecturer(models.Model):
-    name = models.CharField(max_length=255)
-    duration = models.SlugField(unique=True, max_length=255)
-    hp = models.TextField()
-    attack = models.DateTimeField(auto_now_add=True)
-    subject = models.TextField()
-    sprite = models.TextField()
-    pos = models.TextField()
-    lecID = models.TextField()
+    duration = models.IntegerField()
+    name = models.CharField(max_length=100)
+    hp = models.IntegerField()
+    attack = models.IntegerField()
+    sprite = models.CharField(max_length=100)
+    pos = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    wildOrBattle = models.CharField(max_length=100)
+
+    def __str__(self):
+        return (self.name)
--- eugo/views.py
+++ eugo/views.py
@@ -1,8 +1,11 @@
+from black import re
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_object_or_404
+from eugo.models import Lecturer
+from eugo.forms import *
 
 
 def index(request):
@@ -12,7 +15,8 @@ def battle(request):
     return render(request, 'battle.html')
 
 def map(request):
-    return render(request, 'map.html')
+    lec = Lecturer.objects.all()
+    return render(request, 'map.html',{'lec': lec})
 
 def login(request):
     return render(request, 'login.html')
@@ -27,4 +31,29 @@ def lecturerdex(request):
     return render(request, 'lecturerdex.html')
 
 def mapmod(request):
-    return render(request, 'mapmod.html')
+    if request.method == 'POST':
+        print(request.POST)
+        '''
+        form = lecMakerForm(request.POST)
+        if form.is_valid():
+            print("Successfully made new Lecturer event")
+            print(form)
+        else:
+            form = lecMakerForm()
+            print("ERROR! failed to make new lecturer event!")
+            print(form)
+        '''
+        duration = request.POST['duration']
+        name = request.POST['name']
+        hp = request.POST['hp']
+        attack = request.POST['attack']
+        type = request.POST['type']
+        sprite = "teacher_" + str(request.POST['sprite']) + ".png"
+        coords = request.POST['coords']
+        gameop = request.POST['gameop']
+        newLec = Lecturer(duration=duration, name=name, hp=hp, attack=attack, sprite=sprite, pos=coords, type=type, wildOrBattle=gameop)
+        newLec.save()
+        print(newLec)
+
+    lec = Lecturer.objects.all()
+    return render(request, 'mapmod.html',{'lec': lec})
