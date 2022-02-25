c6d382dce3cce348355e298608f76263f0fe43fe
gtarashev <georgitarashev@gmail.com>
2022-02-25 14:21:08 +0000

    added some comments and updated the way that things are done

--- eugo/forms.py
+++ eugo/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 
 class lecMakerForm(forms.Form):
-    duration= forms.IntegerField(label='duration /mins:')
-    name= forms.CharField(label='lecture name')
-    hp= forms.IntegerField(label='Health Points')
-    attack= forms.IntegerField(label='attack')
-    type = forms.CharField(label='Typing')
-    sprite = forms.CharField(label='sprite' )
-    coords= forms.CharField(label='coords')
-    gameOp = forms.CharField(label='battle/spawn?')
+    duration    =   forms.IntegerField(label='duration /mins:')
+    name        =   forms.CharField(label='lecture name')
+    hp          =   forms.IntegerField(label='Health Points')
+    attack      =   forms.IntegerField(label='attack')
+    type        =   forms.CharField(label='Typing')
+    sprite      =   forms.CharField(label='sprite' )
+    coords      =   forms.CharField(label='coords')
+    gameOp      =   forms.CharField(label='battle/spawn?')
--- eugo/urls.py
+++ eugo/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from . import views
 
+""" All of the urls and extensions that we have made, all of them are /eugo/{} """
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('map/', views.map, name='map'),
-    path('battle/', views.battle, name='battle'),
-    path('player/', views.player, name='player'),
-    path('lecturers/', views.lecturers, name='lecturers'),
-    path('lecturerdex/', views.lecturerdex, name='lecturerdex'),
-    path('mapmod/', views.mapmod, name='mapmod'),
+    path('',                views.index,        name='index'),
+    path('login/',          views.login,        name='login'),
+    path('map/',            views.map,          name='map'),
+    path('battle/',         views.battle,       name='battle'),
+    path('player/',         views.player,       name='player'),
+    path('lecturers/',      views.lecturers,    name='lecturers'),
+    path('lecturerdex/',    views.lecturerdex,  name='lecturerdex'),
+    path('mapmod/',         views.mapmod,       name='mapmod'),
 ]
