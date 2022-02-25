1eb81d27a12f1058d8b0376415af6b06886e1d4b
gtarashev <georgitarashev@gmail.com>
2022-02-25 13:57:17 +0000

    added firstname, surname and salt to the player model and updated the database

--- eugo/models.py
+++ eugo/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class Player(models.Model):
-   username = models.CharField(max_length=20)
-   password = models.CharField(max_length=255)
-   is_admin = models.BooleanField(default=False)
+   firstname    =   models.CharField(max_length=30, null=True)
+   surname      =   models.CharField(max_length=30, null=True)
+   username     =   models.CharField(max_length=20)
+   password     =   models.CharField(max_length=255)
+   salt         =   models.CharField(max_length=10, null=True)
+   is_admin     =   models.BooleanField(default=False)
 
    def __str__(self):
        return(self.username)
