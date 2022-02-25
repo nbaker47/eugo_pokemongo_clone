f42b8a372d315f6933e394a6c57fc3a5056400f5
George Southall <72312397+itswift@users.noreply.github.com>
2022-02-25 16:07:55 +0000

    The register sends information to the server. Although it seems to save the information twice in the variable.

--- eugo/urls.py
+++ eugo/urls.py
@@ -4,24 +4,13 @@ from . import views
 
 """ All of the urls and extensions that we have made, all of them are /eugo/{} """
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
-    path('map/', views.map, name='map'),
-    path('battle/', views.battle, name='battle'),
-    path('player/', views.player, name='player'),
-    path('lecturers/', views.lecturers, name='lecturers'),
-    path('lecturerdex/', views.lecturerdex, name='lecturerdex'),
-    path('mapmod/', views.mapmod, name='mapmod'),
-=======
     path('',                views.index,        name='index'),
     path('login/',          views.login,        name='login'),
+    path('register/',       views.register,     name='register'),
     path('map/',            views.map,          name='map'),
     path('battle/',         views.battle,       name='battle'),
     path('player/',         views.player,       name='player'),
     path('lecturers/',      views.lecturers,    name='lecturers'),
     path('lecturerdex/',    views.lecturerdex,  name='lecturerdex'),
     path('mapmod/',         views.mapmod,       name='mapmod'),
->>>>>>> c6d382dce3cce348355e298608f76263f0fe43fe
 ]
